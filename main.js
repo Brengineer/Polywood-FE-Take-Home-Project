e6302d270695503f009bdc529d8b9355c10f4076
Brendan Tull <brendan@tulltribe.org>
2022-06-29 19:14:55 -0500

    Add primary files

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+    polywood.initAll();
+},false);
+
+polywood = {
+    initAll: function() {
+        let accordion = document.getElementById('___');
+    }
+};
